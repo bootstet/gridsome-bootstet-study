072e500e067cd296cac4f1f91ba033f6a9f82182
bootstet <chenyouzeng@tcna.tencent.com>
2020-09-28 22:50:54 +0800

    gridsoma init

---
+++ src/layouts/Default.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <Header >
+        <slot/>
+      </Header>
+      <!-- <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong> -->
+      <!-- <nav class="nav">
+        <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/about/">About</g-link>
+      </nav> -->
+    </header>
+    <slot/>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+import Header from './components/Header'
+export default {
+   data() {
+     return {
+
+     }
+   },
+   components: {
+     Header
+   }
+}
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+}
+
+.layout {
+  /* max-width: 760px; */
+  margin: 0 auto;
+  /* padding-left: 20px; */
+  padding-right: 20px;
+}
+
+.header {
+  /* display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+  border: 1px solid #000; */
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
---
+++ src/layouts/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="page">
+    <h1 class="header_h">bootstet 博客</h1>
+    <h3 class="header_t">欢迎来到bootstet博客</h3>
+    <nav class="nav">
+      <g-link class="nav__link" to="https://github.com/bootstet">Github 主页</g-link>
+      <g-link class="nav__link" to="/about/">博客源码</g-link>
+    </nav>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {
+
+    };
+  },
+  props: {
+    urlSrc: String
+  },
+  components: {},
+  mounted: function () {
+    console.log('---', this.urlSrc)
+  }
+};
+</script>
+
+<style>
+  .page {
+    width: 100%;
+    height: 300px;
+    background-image: linear-gradient(120deg, rgb(38, 144, 249), rgb(252, 45, 45));
+    color: rgb(255, 255, 255);
+    overflow: hidden;
+  }
+  .header_h {
+    text-align: center;
+    margin-top: 60px;
+    overflow: hidden;
+  }
+  .header_t {
+    text-align: center;
+    margin-top: 10px;
+  }
+  .nav {
+    margin: 0 auto;
+    text-align: center;
+  }
+  .nav .nav__link {
+    padding: .75rem 1rem;
+    text-decoration: none;
+    display: inline-block;
+    margin-bottom: 1rem;
+    color: hsla(0,0%,100%,.7);
+    background-color: hsla(0,0%,100%,.08);
+    border: 1px solid hsla(0,0%,100%,.2);
+    border-radius: .3rem;
+    transition: color .2s,background-color .2s,border-color .2s;
+  }
+
+</style>
